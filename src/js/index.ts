cf0d489da30bebd633f6db972a3959618b40e535
Lightman <yaruk.my@gmail.com>
2019-09-16 21:15:16 +0300

    add demo html file

---
+++ src/js/index.ts
@@ -0,0 +1,13 @@
+import Popover from './Popover';
+
+declare var window: any;
+
+window.Mrpopover = Popover;
+
+$.fn.extend({
+    mrpopover: function() {
+        return this.each((i: number, el: HTMLElement) => {
+            Popover.init({$el: $(el)});
+        });
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/index.ts',
+    entry: [
+        './src/js/index.ts',
+        './src/scss/app.scss',
+    ],
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
     resolve: {
         extensions: [
             '.ts'
